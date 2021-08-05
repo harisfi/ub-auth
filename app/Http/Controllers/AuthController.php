c318459a0746a26546c07107a0056993a9955a24
harisfi <acc.cp.haris@gmail.com>
2021-08-05 11:24:19 +0700

    Add authorization

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function auth(Request $request) {
+        try {
+            $authHeader = base64_decode(explode(' ', $request->header('Authorization'))[1]);
+            
+            $nim = explode(':', $authHeader)[0];
+            $password = explode(':', $authHeader)[1];
+            $passport = md5('123ab' . $password) . '_' . $nim;
+            $ip = $_SERVER['REMOTE_ADDR'];
+
+            $url_login = "https://bais.ub.ac.id/api/login/jsonapi/?userid=$nim&passport=$passport&challenge=123ab&appid=EKS1&ipaddr=$ip";
+            $initlogin = curl_init($url_login);
+            curl_setopt($initlogin, CURLOPT_URL, $url_login);
+            curl_setopt($initlogin, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($initlogin, CURLOPT_SSL_VERIFYPEER, false);
+            $execlogin = curl_exec($initlogin);
+            $json = json_decode($execlogin);
+
+            if ($json->AUTHORITY->PASSWD == 1){
+                $response = [
+                    'message' => 'success',
+                    'data' => base64_encode($execlogin)
+                ];
+                return response()->json($response, 200);
+            } else {
+                $response = [
+                    'message' => 'error',
+                    'data' => $json->errormsg
+                ];
+                return response()->json($response, 200);
+            }
+
+        } catch (Exception $ex) {
+            report($ex);
+            return response()->json([
+                'message' => 'error',
+                'data' => $ex->getMessage()
+            ], 400);
+        }
+    }
+}
